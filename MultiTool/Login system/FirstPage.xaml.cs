a6384571129a3d373e32dd70cb39ede0fb920456
Rafael M <un_segundo@hotmail.com>
2017-05-20 17:35:03 +0200

    Login window finished
    Working properly even giving an error message

--- MultiTool/Login system/FirstPage.xaml.cs
+++ MultiTool/Login system/FirstPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,19 +17,58 @@ namespace MultiTool.Login_system {
     /// <summary>
     /// Interaction logic for FirstPage.xaml
     /// </summary>
-    public partial class FirstPage : Window {
+    public partial class FirstPage : Window, INotifyPropertyChanged {
         public FirstPage() {
             InitializeComponent();
+            this.DataContext = this;
         }
 
-        private void MenuLogin(object sender, RoutedEventArgs e) {
-            var s = new MultiTool.Unit_Converter.UnitConverter();
-            s.Show();
+        #region NotifyProperties
+        public String Username
+        {
+            get
+            {
+                return username;
+            }
+            set
+            {
+                username = value;
+            }
         }
+        #endregion
+        #region PropertyChangedNotifier
+        protected virtual void OnPropertyChanged(string name) {
+            if(PropertyChanged != null) {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+        private String username;
+        private String password;
+        private String userAdmin = "admin";
+        private String passAdmin = "admin";
 
         private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e) {
-            var password = PasswordBox.Password;
+            this.password = PasswordBox.Password.ToString();
         }
 
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            if(username == userAdmin && password == passAdmin) {
+                var s = new MultiTool.Unit_Converter.UnitConverter();
+                s.Show();
+            }else {
+                MessageBoxResult result = MessageBox.Show("User and/or password invalid. Press 'Yes' to exit now or 'No' to try again.", "Invalid login", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+                if(result == MessageBoxResult.Yes) {
+                    Application.Current.Shutdown();
+                }
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e) {
+
+        }
     }
 }
